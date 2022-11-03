e841311980043ea204b4286945bde323a39b3170
JoshHyde9 <joshhyde546@gmail.com>
2022-11-04 09:36:29 +1100

    feat: added funcs to handle brand db queries

---
+++ src/services/brand.js
@@ -0,0 +1,25 @@
+import { collection, getDocs, query, where } from "firebase/firestore/lite";
+
+import { db } from "../../util/firebase";
+import { undoFixURLParam } from "../utils/parseURLParam";
+
+export const getAllBrands = async () => {
+  const brandSnapshot = await getDocs(collection(db, "brands"));
+
+  return brandSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
+
+/**
+ * @param {string} brandName
+ */
+export const getBrandItemsByName = async (brandName) => {
+  const param = undoFixURLParam(brandName);
+
+  const bikesRef = collection(db, "bikes");
+
+  const q = query(bikesRef, where("brand", "==", param));
+
+  const querySnapshot = await getDocs(q);
+
+  return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
