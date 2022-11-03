b8b7eae7ffa18fa6a15cbd0cf8e26b36b34c476d
JoshHyde9 <joshhyde546@gmail.com>
2022-11-04 09:35:07 +1100

    feat: added data to pages

--- src/pages/Brand/Brand.jsx
+++ src/pages/Brand/Brand.jsx
@@ -1,9 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getAllBrands } from "../../services/brand";
+import { fixURLParam } from "../../utils/parseURLParam";
 
 export const Brand = () => {
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getAllBrands();
+      setBrands(data);
+    };
+
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>This is where all of the brands are</h1>
+      {brands.map((brand) => {
+        const fixedBrandParam = fixURLParam(brand.name);
+        return (
+          <div key={brand.id}>
+            <Link to={`/brand/${fixedBrandParam}`}>
+              <img src={brand.image_url} alt={brand.name} />
+              <h3>{brand.name}</h3>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Brand/BrandName/BrandName.jsx
+++ src/pages/Brand/BrandName/BrandName.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+// Database
+import { getBrandItemsByName } from "../../../services/brand";
+
+// Components
+import { Card } from "../../../components/Card/Card";
+
 export const BrandName = () => {
+  const { brandName } = useParams();
+
+  const [brandItems, setBrandItems] = useState([]);
+
+  useEffect(() => {
+    const getBrand = async () => {
+      const data = await getBrandItemsByName(brandName);
+
+      setBrandItems(data);
+    };
+
+    getBrand();
+  }, []);
   return (
-    <div>
-      <h1>This is the brand's page</h1>
+    <div className="gallery">
+      {brandItems.map((bike) => (
+        <Card key={bike.id} bike={bike} />
+      ))}
     </div>
   );
 };
