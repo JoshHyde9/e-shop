9f3e7e05c44e278ae7380d670eb083cc23dfd700
JoshHyde9 <joshhyde546@gmail.com>
2022-11-05 15:28:54 +1100

    fix: changed component dir

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import { Home } from "./pages/Home/Home";
-import { Bike } from "./pages/Bike";
+import { Bike } from "./pages/Bike/Bike";
 import { Brand } from "./pages/Brand/Brand";
 import { BrandName } from "./pages/Brand/BrandName/BrandName";
 
