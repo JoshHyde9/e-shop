3211227ecdbc6443c46a45f05d7d0ed386ac692f
JoshHyde9 <joshhyde546@gmail.com>
2022-11-05 15:29:22 +1100

    feat: added function to get bike by id

--- src/services/bike.js
+++ src/services/bike.js
@@ -1,9 +1,28 @@
-import { collection, getDocs } from "firebase/firestore/lite";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore/lite";
 
 import { db } from "../../util/firebase";
 
+/**
+ * Get all of the bikes in the db
+ * @returns
+ */
 export const getAllBiikes = async () => {
   const bikeSnapshot = await getDocs(collection(db, "bikes"));
 
   return bikeSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
+
+/**
+ * Get a bike by id
+ * @param {string} id
+ */
+export const getBikeById = async (id) => {
+  const bikeRef = doc(db, "bikes", id);
+  const bikeSnapshot = await getDoc(bikeRef);
+
+  if (!bikeSnapshot.exists()) {
+    return null;
+  }
+
+  return { id: bikeSnapshot.id, ...bikeSnapshot.data() };
 };
