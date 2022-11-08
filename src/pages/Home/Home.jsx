3466c3b04791f99f1e2e93afd8137287b7c9c986
JoshHyde9 <joshhyde546@gmail.com>
2022-11-08 17:11:13 +1100

    feat: added carousel to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import { getAllBiikes } from "../../services/bike";
 
 // Components
 import { Card } from "../../components/Card/Card";
+import { Carousel } from "../../components/Carousel/Carousel";
 
 // Context
 import { FavouritesContext } from "../../hooks/FavouritesContext";
@@ -16,25 +17,38 @@ export const Home = () => {
   const [, , toggleFavourite, isFavourite] = useContext(FavouritesContext);
 
   const [bikes, setBikes] = useState([]);
+  const [carouselImages, setCarouselImages] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
+      // Get all of the bikes from the db
       const data = await getAllBiikes();
       setBikes(data);
     };
     getData();
   }, []);
 
+  useEffect(() => {
+    // Grab the first 4 items and push to carousel image array state
+    const items = bikes.slice(0, 4);
+    setCarouselImages(items);
+  }, [bikes]);
+
   return (
-    <div className={styles.gallery}>
-      {bikes.map((bike) => (
-        <Card
-          key={bike.id}
-          bike={bike}
-          handleFavourite={toggleFavourite}
-          favourited={isFavourite}
-        />
-      ))}
+    <div>
+      <div className={styles.carousel_container}>
+        {carouselImages.length > 0 && <Carousel images={carouselImages} />}
+      </div>
+      <div className={styles.gallery}>
+        {bikes.map((bike) => (
+          <Card
+            key={bike.id}
+            bike={bike}
+            handleFavourite={toggleFavourite}
+            favourited={isFavourite}
+          />
+        ))}
+      </div>
     </div>
   );
 };
