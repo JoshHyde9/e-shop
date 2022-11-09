562884e5239dcc2e612ea5b6b732b58832893144
JoshHyde9 <joshhyde546@gmail.com>
2022-11-09 17:31:45 +1100

    refactor: moved context so nav has access to it

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 import NavBar from "./components/Layout/Navbar";
+
+import { CartProvider } from "./hooks/CartContext";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <>
+      <CartProvider>
         <NavBar />
         <App />
-      </>
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
