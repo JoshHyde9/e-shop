0981d0d2ae7d63170fa68f1763a8a7b0a87f3c72
JoshHyde9 <joshhyde546@gmail.com>
2022-11-09 17:35:32 +1100

    feat: cart page now does what it is supposed to do

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,48 @@
+import { useState, useEffect, useContext } from "react";
+
+// Context
+import { CartContext } from "../../hooks/CartContext";
+
+// Db
+import { getAllCartItems } from "../../services/cart";
+
+// Components
+import { CartItem } from "../../components/CartItem/CartItem";
+
+import styles from "./Cart.module.scss";
+
 export const Cart = () => {
+  const [cart, , removeFromCart, updateCart] = useContext(CartContext);
+
+  const [dbCart, setDBCart] = useState([]);
+
+  useEffect(() => {
+    const getCartItems = async () => {
+      const data = await getAllCartItems(cart);
+
+      setDBCart(data);
+    };
+
+    getCartItems();
+  }, [cart]);
+
+  if (!dbCart || dbCart.length <= 0) {
+    return <h1 className="error">Items added to your cart will appear here</h1>;
+  }
+
   return (
-    <div>
-      <h1>This is the cart page</h1>
+    <div className={styles.cart_container}>
+      {dbCart.map((bike, i) => {
+        return (
+          <CartItem
+            key={bike.id}
+            bike={bike}
+            localCart={cart[i]}
+            removeFromCart={removeFromCart}
+            updateCart={updateCart}
+          />
+        );
+      })}
     </div>
   );
 };
