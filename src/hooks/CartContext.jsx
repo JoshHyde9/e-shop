b5484b9651959c5407e042659daa1ce164e921e8
JoshHyde9 <joshhyde546@gmail.com>
2022-11-09 14:21:27 +1100

    feat: created basic cart context

---
+++ src/hooks/CartContext.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect, createContext } from "react";
+
+export const CartContext = createContext();
+
+const getInitialState = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : [];
+};
+
+/**
+ * @param {{children: React.ReactNode}} props
+ */
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(getInitialState);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.parse(cart));
+  }, [cart]);
+
+  return (
+    <CartContext.Provider value={[cart, setCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
